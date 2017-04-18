e3fb8e1c63191d049bd69aede05a2901cd82b726
Robert Daly <unknown9595@hotmail.com>
2017-04-18 15:35:40 +0100

    Case 27695; Updated stickyslot to work with multiple slots

--- js/stickyslot_sticky.js
+++ js/stickyslot_sticky.js
@@ -8,52 +8,92 @@
   Drupal.behaviors.stickyslot_sticky = {
 
     attach: function() { 
-        
-      
       var stickyslots = Drupal.settings.stickySlot.slots;
 
+      window.addEventListener('load', function() {
 
-      // The element to stick.
-      stickyslots.forEach(function(slot, index) {  
+        var $body       = $(document.body);
 
-        var stickyElement = stickyslots[index].element;
+        var contentdiv  = $('#content').height();
+        var sidebardiv  = $('#sidebar-second').height();
 
-        /**
-        * Initialise sticky-kit
-        *
-        * @callback
-        */
-       
-        var options = {
-          parent: stickyslots[index].parent,
-          spacer: stickyslots[index].spacer,
-          offset_top: stickyslots[index].offset_top,
-        };
+        if (contentdiv < sidebardiv) {
+          return;            
+        }
 
-        window.onload = function() {          
-          // Test that the sidebar is longer than the content, otherwise disable onScroll functionality. 
-          var contentdiv = $('#content').height();
-          var sidebardiv = $('#sidebar-second').height();
-          if (contentdiv < sidebardiv) {
-            return;            
-          }else{
-            $(stickyElement).stick_in_parent(options);
-            $(document).on('lazyadslot:slotready', function(event){        
-              // Init sticky-kit when a lazy ad slot is loaded.
-              $(stickyElement).stick_in_parent(options);
+        var stickSlots = function() {
+          stickyslots.forEach(function(slot, index) {
+            var $stickyElement = $(slot.element);
+
+            var options = {
+              parent:     slot.parent,
+              spacer:     slot.spacer,
+              offset_top: slot.offset_top
+            };
+
+            if ($stickyElement.length > 0 && !slot.sticky) {
+              $stickyElement.stick_in_parent(options);
+              slot.sticky = true;
+
+              if (index === stickyslots.length - 1) {
+                onBottom($stickyElement);
+              }
+            }
+          });
+        }
+
+        var onBottom = function($stickyElement) {
+          $stickyElement.one('sticky_kit:bottom', function() {
+            var reverseStickySlot = stickyslots.slice().reverse();
+
+            var height = window.getComputedStyle(document.querySelector(reverseStickySlot[0].element)).height;
+
+            reverseStickySlot.shift();
+
+            reverseStickySlot.forEach(function(slot, index) {
+              document.querySelector(slot.element).style.paddingBottom = height;
             });
-          }
-        };
 
-        // For ad slots: wait until the ad slot is loaded first
+            $body.trigger('sticky_kit:recalc');
+
+            offBottom($stickyElement);
+          });
+        }
+
+        var offBottom = function($stickyElement) {
+          $stickyElement.one('sticky_kit:unbottom', function() {
+            var reverseStickySlot = stickyslots.slice().reverse();
+            reverseStickySlot.shift();
+
+            reverseStickySlot.forEach(function(slot, index) {
+              document.querySelector(slot.element).style.removeProperty('padding-bottom');
+            });
+
+            $body.trigger('sticky_kit:recalc');
+
+            var $el = $(stickyslots[0].element);
+
+            $el.off('sticky_kit:unstick');
+            $el.one('sticky_kit:unstick', function() {
+              $body.trigger('sticky_kit:recalc');
+            });
+
+            onBottom($stickyElement);
+          });
+        }
+
         googletag.cmd.push(function() {
           googletag.pubads().addEventListener('slotRenderEnded', function(e) {
+            stickSlots();
+
             if ((e.slot === googletag.slots.stickyslot_0) && (e.isEmpty === false)) { 
-              $(document.body).trigger('sticky_kit:recalc');           
+              $body.trigger('sticky_kit:recalc');           
             }
           });
         });
-      }); 
+
+        stickSlots();
+      });
     }
   }
 })(jQuery);
